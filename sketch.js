c4bc5efcde3e532bacbde609a93782d98ef8450f
Mommocmoc <mommocmoc@gmail.com>
2018-03-11 23:34:19 +0900

    testing

--- sketch.js
+++ sketch.js
@@ -1,14 +1,38 @@
 var s ;
+var shapeColor ;
+var preS = 0 ;
+var curS ;
+var input ;
 function setup() {
- createCanvas(displayWidth,displayHeight);
+  createElement('h1',"비글이 두마리에게 보내는 메시지");
+input = createInput('비글이 두마리를 축하해주세요.');
+ createCanvas(windowWidth,windowHeight);
+
+ shapeColor = color(200,0,0);
 	//createCanvas(windowWith, 500);
 }
 
 function draw() {
-	background(0,126,255);
+  clear();
+	//background(0,126,255);
 	s=50+second()*10;
+  text(input.value(),100,100,32);
+  curS = millis();
 	noStroke();
-	ellipse(s,circle.y,circle.diameter,circle.diameter);
+  fill(shapeColor)
+	ellipse(windowWidth/2,windowHeight/2,10+s,10+s);
+  if(curS-preS > 1000){
+    preS = curS ;
+    colorChange();
+  }
+}
+
+function mousePressed(){
+  colorChange();
+}
+
+function colorChange(){
+  shapeColor = color(random(255),random(255),random(255))
 }
 
 var circle = {
@@ -16,3 +40,7 @@ var circle = {
 	y : 200,
 	diameter :50
 };
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
